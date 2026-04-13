874aa69ae4baf7e54cf62cf8ee2e70a555fb9148
JorgeWendell <jorge.wendell@adelbr.tech>
2026-04-13 18:09:45 -0300

    feat:add booking item component

---
+++ app/components/booking-item.tsx
@@ -0,0 +1,42 @@
+import { Avatar,  AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Card } from "@/components/ui/card";
+
+
+
+interface BookingItemProps {
+    serviceName: string;
+    babershopName: string;
+    babershopImageUrl: string;
+    date: string;
+}
+
+const BookingItem = ({ serviceName, babershopName, babershopImageUrl, date }: BookingItemProps) => {
+    return ( 
+   <Card className="flex flex-row items-center justify-between w-ful min-w-full p-0">
+    {/*Esquerda*/}
+    <div className="flex flex-col gap-4 flex-1 p-4">
+        <Badge>Confirmado</Badge>
+        <div className="flex flex-col gap-2">
+            <p className="font-bold">{serviceName}</p>
+            <div className="flex items-center gap-2 5">
+                <Avatar className="h-6 w-6">
+                    <AvatarImage src={babershopImageUrl} />                    
+                </Avatar>
+                <p className="text-sm text-muted-foreground">{babershopName}</p>
+            </div>
+        </div>
+    </div>
+    {/*Direita*/}
+    <div className="flex flex-col items-center justify-center p-6 h-full border-l py-3">
+        <p className="text-xs capitalize">{date.toLocaleDateString('pt-BR', {month: "long"})}</p>
+        <p>{date.toLocaleDateString('pt-BR', {day: "2-digit"})}</p>
+        <p>{date.toLocaleTimeString('pt-BR', {hour: "2-digit", minute: "2-digit"})}</p>
+
+    </div>
+   </Card>
+
+);
+}
+ 
+export default BookingItem; 
--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,24 @@ import Header from "./components/header";
 import SearchInput from "./components/search-input";
 import Image from "next/image";
 import hero from "@/public/hero.png";
+import BookingItem from "./components/booking-item";
 
 export default function Home() {
   return (
-    <div>
+    <main>
       <Header />
       <div className="px-5 space-y-4">    
           <SearchInput />     
-          <Image src={hero} alt="Belezy" sizes="100vw" className="w-full h-auto rounded-lg" />  
+          <Image src={hero} alt="Belezy" sizes="100vw" className="w-full h-auto rounded-lg" /> 
+          <h2 className="text-xs text-foreground font-semibold uppercase">Agendamentos</h2> 
+          <BookingItem 
+           serviceName="Corte de cabelo"
+           babershopName="Barbearia do João"
+           babershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+           date={new Date()}
+           />
       </div>
       
-    </div>
+    </main>
   );
 }
